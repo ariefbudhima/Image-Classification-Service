77a7fdc5e818e5c348bf5ff109524acf86c4d00a
ariefbudhima <ariefbudhiman1997@gmail.com>
2023-02-19 14:56:16 +0700

    cleaning code

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Image-Classification-Service
 
+# Convert model .h5 to .pb for golang to load using google colab
+
+# Get first layer from model and last layer from model using `saved_model_cli` using this command `saved_model_cli show --dir /path/to/saved_model --all`
+
 # install dependencies
 go get github.com/galeone/tfgo
 go get github.com/gin-gonic/gin
--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	"image/png"
 	"math"
 	"net/http"
-	"reflect"
-	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
 	"github.com/nfnt/resize"
@@ -39,7 +37,6 @@ func classifyHandler(c *gin.Context) {
 	}
 
 	// // preprocess the image
-	// img = imaging.Resize(img, 224, 224, imaging.Lanczos)
 	resized := resize.Resize(uint(imageSize), uint(imageSize), img, resize.MitchellNetravali)
 
 	// Center crop the image
@@ -69,14 +66,6 @@ func classifyHandler(c *gin.Context) {
 	// Create a tensor from the preprocessed input
 	imageTensor, _ := tf.NewTensor(tfImage)
 
-	imgTf, err := imageToTensor(img, 224, 224)
-	fmt.Println("==============================================================================================================================")
-	fmt.Println(imgTf)
-	fmt.Println(imageTensor)
-	// fmt.Println(inputVals)
-
-	// imageTensor, err := tensorFromImage
-
 	// Load model
 	model := tfgo.LoadModel("saved_model", []string{"serve"}, nil)
 
@@ -87,59 +76,11 @@ func classifyHandler(c *gin.Context) {
 	probabilities := output[0].Value().([][]float32)[0]
 
 	softmax_classify := softmax(probabilities)
-	sigmoid_classify := sigmoid(probabilities)
-	reluClassify := relu(probabilities)
-	tanhClassify := tanh(probabilities)
-	fmt.Println(reflect.TypeOf(output[0].Value()))
-	fmt.Println(output[0].Value())
-	// Print the result
-	fmt.Printf("Result: %v\n", probabilities)
-	fmt.Println("softmax: ", softmax_classify)
-	fmt.Println("sigmoid: ", sigmoid_classify)
-	fmt.Println("ReLu: ", reluClassify)
-	fmt.Println("Tanh: ", tanhClassify)
-
-	fmt.Println(imageTensor)
 	if softmax_classify[1] < 0.2694 {
-		// if softmax_classify[1] < 0.271 {
 		c.JSON(http.StatusOK, gin.H{"result": "kolam"})
 	} else {
 		c.JSON(http.StatusOK, gin.H{"result": "bukan kolam"})
 	}
-
-	// c.JSON(http.StatusOK, gin.H{"result": "image sent successfully"})
-}
-
-func imageToTensor(img image.Image, imageHeight, imageWidth int) (tfTensor *tf.Tensor, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("classify: %s (panic)\nstack: %s", r, debug.Stack())
-		}
-	}()
-
-	if imageHeight <= 0 || imageWidth <= 0 {
-		return tfTensor, fmt.Errorf("classify: image width and height must be > 0")
-	}
-
-	var tfImage [1][][][3]float32
-
-	for j := 0; j < imageHeight; j++ {
-		tfImage[0] = append(tfImage[0], make([][3]float32, imageWidth))
-	}
-
-	for i := 0; i < imageWidth; i++ {
-		for j := 0; j < imageHeight; j++ {
-			r, g, b, _ := img.At(i, j).RGBA()
-			tfImage[0][j][i][0] = convertValue(r)
-			tfImage[0][j][i][1] = convertValue(g)
-			tfImage[0][j][i][2] = convertValue(b)
-		}
-	}
-	return tf.NewTensor(tfImage)
-}
-
-func convertValue(value uint32) float32 {
-	return (float32(value >> 8)) / float32(255)
 }
 
 func softmax(input []float32) []float32 {
@@ -151,34 +92,6 @@ func softmax(input []float32) []float32 {
 	}
 	for i := range output {
 		output[i] /= sum
-	}
-	return output
-}
-
-func sigmoid(input []float32) []float32 {
-	var predictions []float32
-	// Apply sigmoid to the predictions
-	for _, rawPrediction := range input {
-		prediction := 1.0 / (1.0 + math.Exp(float64(-rawPrediction)))
-		predictions = append(predictions, float32(prediction))
-	}
-
-	return predictions
-}
-
-func relu(input []float32) []float32 {
-	for i := range input {
-		if input[i] < 0 {
-			input[i] = 0
-		}
-	}
-	return input
-}
-
-func tanh(input []float32) []float32 {
-	output := make([]float32, len(input))
-	for i, val := range input {
-		output[i] = float32(math.Tanh(float64(val)))
 	}
 	return output
 }
